8d54365d5568b7d49e29a939b46638265c69abc5
Luana Porto <luanaporto@usf.edu>
2025-12-06 18:48:30 -0500

    imported pandas and created. a function to load data data/raw

---
+++ src/data_cleaning.py
@@ -0,0 +1,19 @@
+#import pandas 
+import pandas as pd
+
+#Load sales_data_raw.csv from data/raw/
+
+def load_data(file_path:str):
+    """Load data from a CSV file into a pandas DataFrame."""
+    try:
+        df = pd.read_csv(file_path)
+        print(f"Data loaded successfully from {file_path}")
+        return df
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return pd.DataFrame
+
+#Clean the data
+#Github Copilot was used in all the code below
+
+
